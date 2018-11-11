57a8309bc3aeb32e6f2104bbe3e8786419f0cc22
Cryptic Loop <nickstride1@gmail.com>
2018-11-11 12:09:40 +0000

    Function creation and refactoring

--- 3_domGame/app.js
+++ 3_domGame/app.js
@@ -25,6 +25,12 @@ function init() {
 
   document.getElementById('name-0').textContent = 'Player 1';
   document.getElementById('name-1').textContent = 'Player 2';
+  document.querySelector('.player-0-panel').classList.remove('winner');
+  document.querySelector('.player-1-panel').classList.remove('winner');
+  document.querySelector('.player-0-panel').classList.remove('active');
+  document.querySelector('.player-1-panel').classList.remove('active');
+
+  document.querySelector('.player-0-panel').classList.add('active');
 };
 
 function changeActivePlayer() {
