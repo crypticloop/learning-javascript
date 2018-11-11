16fa570caaf7ceafe60503c3b54adeeedff81564
Cryptic Loop <nickstride1@gmail.com>
2018-11-11 09:58:06 +0000

    Finished core game, renamed

--- 3_domGame/app.js
+++ 3_domGame/app.js
@@ -10,21 +10,36 @@ GAME RULES:
 */
 let scores, roundScore, activePlayer;
 
-scores = [0,0];
-roundScore = 0;
-activePlayer = 0;
+function init() {
+  scores = [0,0];
+  roundScore = 0;
+  activePlayer = 0;
 
+  document.querySelector('.dice').style.display = 'none';
 
-// document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
+  document.getElementById('score-0').textContent = '0';
+  document.getElementById('score-1').textContent = '0';
+  document.getElementById('current-0').textContent = '0';
+  document.getElementById('current-1').textContent = '0';
 
-document.querySelector('.dice').style.display = 'none';
+  document.getElementById('name-0').textContent = 'Player 1';
+  document.getElementById('name-1').textContent = 'Player 2';
+};
 
-document.getElementById('score-0').textContent = '0';
-document.getElementById('score-1').textContent = '0';
-document.getElementById('current-0').textContent = '0';
-document.getElementById('current-1').textContent = '0';
+function changeActivePlayer() {
+  activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+  roundScore = 0;
 
-function btn() {
+  document.getElementById('current-0').textContent = '0';
+  document.getElementById('current-1').textContent = '0';
+
+  document.querySelector('.player-0-panel').classList.toggle('active');
+  document.querySelector('.player-1-panel').classList.toggle('active');
+
+  document.querySelector('.dice').style.display = 'none';
+};
+
+function roll() {
   let dice = Math.floor(Math.random() * 6) + 1;
 
   let diceDOM = document.querySelector('.dice');
@@ -37,16 +52,25 @@ function btn() {
     document.querySelector('#current-' + activePlayer).textContent = roundScore;
   } else {
     // change player
-    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
-    roundScore = 0;
-
-    document.getElementById('current-0').textContent = '0';
-    document.getElementById('current-1').textContent = '0';
-
-
-    document.querySelector('.player-0-panel').classList.remove('active');
-    document.querySelector('.player-1-panel').classList.add('active');
+    changeActivePlayer();
   }
-}
+};
 
-document.querySelector('.btn-roll').addEventListener('click', btn)
+function hold() {
+  scores[activePlayer] += roundScore;
+  document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+
+  if (scores[activePlayer] >= 100) {
+    document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
+    document.querySelector('.dice').style.display = 'none';
+    document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+    document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+  } else {
+    changeActivePlayer();
+  }
+};
+
+init();
+document.querySelector('.btn-roll').addEventListener('click', roll);
+document.querySelector('.btn-hold').addEventListener('click', hold);
+document.querySelector('.btn-new').addEventListener('click', init);
