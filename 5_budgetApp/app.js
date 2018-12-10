3db86a6d57cc18e288f8aa7788458ff82edd160c
Cryptic Loop <nickstride1@gmail.com>
2018-12-10 13:30:55 +0000

    Enter button working with click and keyboard return

--- 5_budgetApp/app.js
+++ 5_budgetApp/app.js
@@ -12,8 +12,24 @@ let UIController = (function() {
 
 let controller = (function(budgetCtrl, UICtrl) {
 
-  document.querySelector('.add__btn').addEventListener('click', function() {
-    console.log('Button was clicked');
+  let ctrlAddItem = function() {
+    // get input data
+    // add item to the budget controller
+    // add item to the UI
+    // calculatek the budget
+    // display the budget on the UI
+
+    console.log("it works")
+  }
+
+  document.querySelector('.add__btn').addEventListener('click', ctrlAddItem)
+
+  document.addEventListener('keypress', function(event) {
+    if(event.keyCode == 13 || event.which == 13)  {
+
+      ctrlAddItem();
+
+    }
   })
 
 })(budgetController, UIController);
